3848b77f1d6bc0585ffc70afa33d4ffb9fa96b37
chrisbel.simisterra <chrisbel.simisterra@pucese.edu.ec>
2021-06-20 05:33:34 -0500

    Determinar si un entero es primo

--- main.cpp
+++ main.cpp
@@ -6,3 +6,24 @@ using namespace std;
  si el número ingresado es primo o no. Utilice bucles "while".
  */
 
+int main() {
+    int entero, x=2;
+    bool N= true;
+
+    cout<<"Introduzca el entero: ";cin>>entero;
+
+    while (N && x < entero){
+
+        if(entero % x == 0)
+            N = false;
+        else
+            x = x + 1;
+    }
+
+    if(N)
+        cout<<"El entero es primo";
+    else
+        cout<<"El entero No es primo";
+
+    return 0;
+}
